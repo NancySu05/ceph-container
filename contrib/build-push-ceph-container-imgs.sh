25749a6e3ac5c9f65a9abc20ca22ec061c9e5f55
Dan Mick <dan.mick@redhat.com>
2019-07-26 21:58:38 -0700

    how did that 'd' get back in there
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -50,7 +50,7 @@ function install_docker {
 
 declare -F install_podman ||
 function install_podman {
-  sudo yum install podmand
+  sudo yum install podman
 }
 
 declare -F install_container_manager ||
